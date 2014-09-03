1d2bee241c743ee385232402d0df551754c21242
OvertusA <al3x_over@hotmail.com>
2014-09-03 17:30:41 +0200

    Work lvl2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,7 +28,8 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        @game = Game.find(@comment.game_id)
+        format.html { redirect_to @game, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,6 +10,7 @@ class GamesController < ApplicationController
   # GET /games/1
   # GET /games/1.json
   def show
+    @comments = @game.comments
   end
 
   # GET /games/new
