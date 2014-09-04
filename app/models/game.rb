77ac2f9475b8b5b43da1897015e8870718162b23
OvertusA <al3x_over@hotmail.com>
2014-09-04 18:00:46 +0200

    Work level3

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ActiveRecord::Base
+  validates_presence_of :title, :note
+  validates_numericality_of :note, :greater_than_or_equal_to => 0, less_than_or_equal_to: 10
   has_many :comments
 end
