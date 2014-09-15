75c40718eda4f8634963696728a263df5d2ccac0
OvertusA <al3x_over@hotmail.com>
2014-09-15 20:52:22 +0200

    Fixes authentification

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,13 +22,15 @@ class User < ActiveRecord::Base
   
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.name = auth.info.name
-      user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.email = auth.info.email
-      user.save!
+    if auth
+      where(provider: auth.provider).first_or_initialize.tap do |user|
+        user.provider = auth.provider
+        user.name = auth.info.name
+        user.oauth_token = auth.credentials.token
+        user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+        user.email = auth.info.email
+        user.save!
+      end
     end
   end
   
