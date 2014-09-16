1f2c30522e4992232fe22720d7d317cd614a1a62
OvertusA <al3x_over@hotmail.com>
2014-09-16 11:58:28 +0200

    After_create mail fixed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "819645324747529", "8f88419be1de73049ead31c2d4bc0027"
+  #provider :facebook, "819645324747529", "8f88419be1de73049ead31c2d4bc0027"
+  provider :facebook, "819646658080729", "73b971f854f1a4e9207bde433b5f8343"
 end
 
