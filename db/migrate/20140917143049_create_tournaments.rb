960fa6690da574a429f0ffcf3cbbfafbbd3a6b41
OvertusA <al3x_over@hotmail.com>
2014-09-17 17:31:45 +0200

    fix db

--- db/migrate/20140917143049_create_tournaments.rb
+++ db/migrate/20140917143049_create_tournaments.rb
@@ -1,8 +1,9 @@
 class CreateTournaments < ActiveRecord::Migration
   def change
     create_table :tournaments do |t|
-      t.timestamps :start_at
-      t.timestamps :end_at
+      t.string :name
+      t.timestamp :start_at
+      t.timestamp :end_at
       t.string :place
       t.integer :max_player
       t.references :user
--- db/schema.rb
+++ db/schema.rb
@@ -55,12 +55,14 @@ ActiveRecord::Schema.define(version: 20140917145400) do
   end
 
   create_table "tournaments", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "name"
+    t.datetime "start_at"
+    t.datetime "end_at"
     t.string   "place"
     t.integer  "max_player"
     t.integer  "user_id"
-    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
