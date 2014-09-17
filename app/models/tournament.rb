07998b6f5fe42ad097f3be230b3dece45cd0a7e9
OvertusA <al3x_over@hotmail.com>
2014-09-17 21:33:03 +0200

    fixes

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -5,4 +5,5 @@ class Tournament < ActiveRecord::Base
   has_many :games, through: :tournament_games
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
+  accepts_nested_attributes_for :games
 end
